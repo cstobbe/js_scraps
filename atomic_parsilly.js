62c3ddffe7f8fe6a0b1d3e2adc12503bb1c56be8
cstobbe <carolyn.stobbe@gmail.com>
2017-06-27 20:13:01 -0500

    Make the linter happier

--- atomic_parsilly.js
+++ atomic_parsilly.js
@@ -1,14 +1,16 @@
-function power(base,exponent) {
-  if (exponent == 0) {
-    return 1;
+function power (base, exponent) {
+  if (exponent === 0) {
+    return 1
   } else {
-    return base * power(base, exponent - 1);
+    return base * power(base, exponent - 1)
   }
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply (number1, number2) {
+  return number1 * number2
 }
 
+var a = multiply(2, 3)
+var b = multiply(4, 5)
 
-console.log(multiply(5,2))
+console.log(multiply(a, b))
