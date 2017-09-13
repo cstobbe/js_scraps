37a9288c5c93f6e25dfcf266343cb18a57601bbc
cstobbe <carolyn.stobbe@gmail.com>
2017-09-13 08:36:04 -0500

    Eloquent JS Recursion Example Chapter 3

--- javaScripting.js
+++ javaScripting.js
@@ -76,3 +76,18 @@ function min (a,b) {
     return a;
     else return b;
 }
+console.log(min(0,13));
+
+
+/*Recursion Example*/
+function isEven(n){
+    if (n=0)
+      return true;
+    else if (n=1)
+      return false;
+    else if(n<0)
+      return isEven(-n);
+    else
+      return isEven(n-2);
+    }
+console.log(isEven(81));
