87dbb4fdefa8541a2f9da4c8cd013f1eac8c071b
cstobbe <carolyn.stobbe@gmail.com>
2017-09-18 09:04:55 -0500

    Eloquent JS Ch 2 Chess Board Exercise

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -15,12 +15,12 @@ while (counter<10) {
 }
 console.log(result);*/
 
-/*Chapter 2 Exercise 1*/
+/*Chapter 2 Exercise 1
 
 for (var line="#";line.length<8;line +="#")
-  console.log(line);
+  console.log(line);*/
 
-/*Chapter 2 FizzBuzz exercise*/
+/*Chapter 2 FizzBuzz exercise
 for (var n=1;n<=100; n++){
   var output = "";
   if (n %3  == 0)
@@ -29,4 +29,19 @@ for (var n=1;n<=100; n++){
     output +="buzz";
 
   console.log(output || n);
-}
+}*/
+
+/* Chapter 2 Chess Board Exercise*/
+var size=8;
+var board=" ";
+  for (var y=0;y<size;y++){
+    for (var x=0; x<size;x++){
+    if((x+y)% 2 == 0)
+      board +=" ";
+    else
+      board+="#";
+    }
+    board +="\n";
+  }
+
+console.log(board);
