5b5398e9a773c39de8a4118674c9330eb1757d25
cstobbe <carolyn.stobbe@gmail.com>
2017-09-18 08:36:58 -0500

    eloquent JS Ch2 Exercises

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -1,7 +1,21 @@
 /*Chapter 1 Exercises*/
-/*page 31*/
+/*page 31
 var number = 0;
 while (number<=12) {
   console.log(number);
   number = number + 2;
 }
+
+/*page 32
+var result = 1;
+var counter = 0;
+while (counter<10) {
+  result = result*2;
+  counter = counter + 1;
+}
+console.log(result);*/
+
+/*Chapter 2 Exercise 1*/
+
+for (var line="#";line.length<8;line +="#")
+  console.log(line);
