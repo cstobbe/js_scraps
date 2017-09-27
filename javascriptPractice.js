d6556ee9f51d9ddd0dd049d290b18ce9c6c58275
cstobbe <carolyn.stobbe@gmail.com>
2017-09-27 08:11:59 -0500

    add javascriptPractice.js to repository

---
+++ javascriptPractice.js
@@ -0,0 +1,9 @@
+//p.64
+var journal = [
+  {events: ["work", "touched tree", "pizza", "running", "television"],
+    squirrel: false},
+  {events: ["work", "ice cream", "cauliflower"],
+    squirrel: false},
+  {events: ["weekend", "peanuts", "beer"],
+    squirrel: true},
+];
