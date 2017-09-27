436b611705b2e411443f041d49ce993a030c9e7c
cstobbe <carolyn.stobbe@gmail.com>
2017-09-27 08:46:51 -0500

    save examples

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1,9 +1,13 @@
 //p.64
-var journal = [
-  {events: ["work", "touched tree", "pizza", "running", "television"],
-    squirrel: false},
-  {events: ["work", "ice cream", "cauliflower"],
-    squirrel: false},
-  {events: ["weekend", "peanuts", "beer"],
-    squirrel: true},
-];
+var anObject = {left:1, right:2};
+console.log(anObject.left);
+
+//p.65
+//var journal = [
+  //{events: ["work", "touched tree", "pizza", "running", "television"],
+    //squirrel: false},
+  //{events: ["work", "ice cream", "cauliflower"],
+    //squirrel: false},
+  //{events: ["weekend", "peanuts", "beer"],
+    //squirrel: true},
+//];
