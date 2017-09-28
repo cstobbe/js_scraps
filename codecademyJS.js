91df7c0d93cfc7e5b66c4e2f2829ddfdcac44cb5
cstobbe <carolyn.stobbe@gmail.com>
2017-09-28 08:21:49 -0500

    add codecademy practice problems

--- codecademyJS.js
+++ codecademyJS.js
@@ -1,8 +1,15 @@
 //Kelvin Weather
-const kelvin = prompt('What is the Kelvin temperature today?');
+//const kelvin = prompt('What is the Kelvin temperature today?');
 //Taking Kelvin and converting it to celsius
-const celsius = kelvin-273;
+//const celsius = kelvin-273;
 //taking Celsius and converting to farenheit
-let farenheit = celsius * (9/5) + 32;
-farenheit = Math.floor(farenheit);
-console.log("The temperature is " + farenheit + " degrees farenheit.");
+//let farenheit = celsius * (9/5) + 32;
+//farenheit = Math.floor(farenheit);
+
+//console.log("The temperature is " + farenheit + " degrees farenheit.");
+
+//functions
+const takeOrder = ()=> {
+  console.log('Order: pizza');
+};
+takeOrder();
